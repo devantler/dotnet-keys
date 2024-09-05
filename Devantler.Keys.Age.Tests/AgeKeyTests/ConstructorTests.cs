553dfe45838daf088b06a63001a1dc306b8ba36a
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-09-05 12:18:44 +0200

    Update agekey to support creating key from raw key

--- Devantler.Keys.Age.Tests/AgeKeyTests/ConstructorTests.cs
+++ Devantler.Keys.Age.Tests/AgeKeyTests/ConstructorTests.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Devantler.Keys.Age.Utils;
 
 namespace Devantler.Keys.Age.Tests.AgeKeyTests;
 
@@ -7,6 +8,55 @@ namespace Devantler.Keys.Age.Tests.AgeKeyTests;
 /// </summary>
 public class ConstructorTests
 {
+  /// <summary>
+  /// Tests that an age key is created with valid data.
+  /// </summary>
+  [Fact]
+  public void Constructor_GivenValidRawKey_ShouldCreateAgeKey()
+  {
+    // Arrange
+    var createdAt = DateTime.SpecifyKind(DateTime.UnixEpoch.AddDays(18400), DateTimeKind.Utc);
+    string publicKey = "age1wrczv4ll5att0mm8tmp4052z4fadw5zefxvefuqxu8rqtpe47chskk9dgn";
+    string privateKey = "AGE-SECRET-KEY-1PW4MMMJ9KMZ94C2N2FM3UPLPQPEF8G9QHXP8VX6V6GW3EN9QMSVQX0ATHQ";
+    string rawKey = $"""
+      # created: {DateTimeFormatter.FormatDateTimeWithCustomOffset(createdAt)}
+      # public key: {publicKey}
+      {privateKey}
+      """;
+
+    // Act
+    var ageKey = new AgeKey(rawKey);
+
+    // Assert
+    Assert.Equal(createdAt, ageKey.CreatedAt);
+    Assert.Equal(publicKey, ageKey.PublicKey);
+    Assert.Equal(privateKey, ageKey.PrivateKey);
+    Assert.Equal(rawKey, ageKey.ToString());
+  }
+
+  /// <summary>
+  /// Tests that an age key is created with valid data.
+  /// </summary>
+  [Fact]
+  public void Constructor_GivenInvalidRawKey_ShouldThrowException()
+  {
+    // Arrange
+    var createdAt = DateTime.SpecifyKind(DateTime.UnixEpoch.AddDays(18400), DateTimeKind.Utc);
+    string publicKey = "age1wrczv4ll5att0mm8tmp4052dont-expect-this-to-workfxvefuqxu8rqtpe47chskk9dgn";
+    string privateKey = "AGE-SECRET-KEY-1PW4MMMJ9KMZ94C2PEFdont-expect-this-to-work8G9QHXP8VX6V6GW3EN9QMSVQX0ATHQ";
+    string rawKey = $"""
+      # created: {DateTimeFormatter.FormatDateTimeWithCustomOffset(createdAt)}
+      # public key: {publicKey}
+      {privateKey}
+      """;
+
+    // Act
+    void act() => _ = new AgeKey(rawKey);
+
+    // Assert
+    _ = Assert.Throws<ValidationException>(act);
+  }
+
   /// <summary>
   /// Tests that an age key is created with valid data.
   /// </summary>
